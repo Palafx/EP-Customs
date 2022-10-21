3f24606bc6901a69e5401560874fcfd3d8477e71
Palafx <115953523+Palafx@users.noreply.github.com>
2022-10-20 22:33:57 -0500

    Added, fixed and reverted cards
    - Added "Adaptive Trait - Acclimation".
    - "Lithos, The Adaptive Shadow": Now only adds from Deck when Normal Summoned.
    - "Lithos, The Adaptive Shadow" buff: Now banishes 2 cards instead of 1.
    - "Elderax, The Adaptive Entity": Now cannot be Normal Summoned/Set, as intended.
    - "Elderax, The Adaptive Entity": Updated obsolete function.
    - "Adaptive Blood": Can target "Adaptive" Fusion Monsters once again to work with "Adaptive Trait - Acclimation".

---
+++ script/c1177022.lua
@@ -0,0 +1,97 @@
+--- Elderax, The Adaptive Entity
+local s,id=GetID()
+function s.initial_effect(c)
+--special summon condition
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	c:RegisterEffect(e1)
+--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SPSUMMON_PROC)
+	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+--atkup
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetOperation(s.op)
+	c:RegisterEffect(e3)
+--cannot be target
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetValue(aux.tgoval)
+	c:RegisterEffect(e4)
+--cannot be material
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e5:SetCode(EFFECT_CANNOT_BE_MATERIAL)
+	e5:SetValue(aux.cannotmatfilter(SUMMON_TYPE_FUSION,SUMMON_TYPE_SYNCHRO,SUMMON_TYPE_XYZ,SUMMON_TYPE_LINK))
+	c:RegisterEffect(e5)
+end
+--special summon
+function s.spfilter(c,tp)
+	return c:IsRace(RACE_DRAGON) and c:IsSetCard(0x499) and c:IsAbleToRemoveAsCost() and (Duel.GetLocationCount(tp,LOCATION_MZONE)>0 or (c:IsLocation(LOCATION_MZONE) and c:GetSequence()<5))
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=e:GetHandlerPlayer()
+	local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil,tp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	return ft>-1 and #rg>0 and aux.SelectUnselectGroup(rg,e,tp,3,3,nil,0)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local c=e:GetHandler()
+	local g=nil
+	local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil,tp)
+	local g=aux.SelectUnselectGroup(rg,e,tp,3,3,nil,1,tp,HINTMSG_REMOVE,nil,nil,true)
+	if #g>0 then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+  if not g then return end
+  local tc=g:GetFirst()
+  local atk=0
+  for tc in aux.Next(g) do
+    local catk=tc:GetBaseAttack()
+    if catk<0 then catk=0 end
+    atk=atk+catk
+  end
+  e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1,atk)
+  Duel.Remove(g,POS_FACEUP,REASON_COST)
+  g:DeleteGroup()
+end
+--atk
+function s.atkfilter(c,tp)
+	return c:IsRace(RACE_DRAGON) and c:IsSetCard(0x499)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(s.atkfilter),tp,LOCATION_REMOVED,0,nil)
+	if #g>0 and c:IsFaceup() and c:IsRelateToEffect(e) then
+		local atk=g:GetSum(Card.GetAttack)
+		local e5=Effect.CreateEffect(c)
+		e5:SetType(EFFECT_TYPE_SINGLE)
+		e5:SetCode(EFFECT_UPDATE_ATTACK)
+		e5:SetValue(atk)
+		e5:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		c:RegisterEffect(e5)
+	end
+end
--- script/c1177031.lua
+++ script/c1177031.lua
@@ -15,7 +15,7 @@ function s.initial_effect(c)
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
 	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetHintTiming(0,TIMING_MAIN_END)
+	e3:SetHintTiming(0,TIMING_PHASE_END)
 	e3:SetRange(LOCATION_SZONE)
 	e3:SetCountLimit(1)
 	e3:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
@@ -34,7 +34,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 function s.lvfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x499) and not c:IsType(TYPE_FUSION)
+	return c:IsFaceup() and c:IsSetCard(0x499)
 end
 function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.lvfilter(chkc) end
