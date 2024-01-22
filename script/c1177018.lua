7e9cece991fb1dd9f698c4421babcf1617599beb
Enrique Palafox <115953523+Palafx@users.noreply.github.com>
2024-01-22 11:31:29 -0600

    Update c1177018.lua

--- script/c1177018.lua
+++ script/c1177018.lua
@@ -16,12 +16,14 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--Cannot Special Summon from Extra Deck
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetCategory(CATEGORY_DISABLE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e2:SetTargetRange(0,1)
-	e2:SetTarget(s.splimit)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER)
+	e2:SetCountLimit(1)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
 	c:RegisterEffect(e2)
 end
 s.listed_series={0x499}
@@ -31,14 +33,45 @@ function s.unifilter(c)
 	return c:IsType(TYPE_FUSION) and c:IsSetCard(0x499)
 end
 --cannot sp summon	
-function s.splimit(e,c)
-	if e:GetHandler():GetFlagEffect(id)>0 then e:GetHandler():ResetFlagEffect(id) return false end
-	return c:IsLocation(LOCATION_EXTRA)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CARDTYPE)
+	Duel.SetTargetParam(Duel.SelectOption(tp,1056,1063,1073,1074,1057))
 end
---fusion materials
-function s.ffilter1(c,fc,sumtype,tp)
-	return c:IsAttribute(ATTRIBUTE_LIGHT,fc,sumtype,tp) and c:IsSetCard(0x499,fc,sumtype,tp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local opt=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+	local ct=nil
+	if opt==0 then ct=TYPE_FUSION end
+	if opt==1 then ct=TYPE_SYNCHRO end
+	if opt==2 then ct=TYPE_XYZ end
+	if opt==3 then ct=TYPE_LINK end
+	--Cannot Special Summon monsters of the declared type
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,5))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,1)
+	e1:SetTarget(s.sumlimit)
+	e1:SetLabel(ct)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	--Negate the effects of monsters of that type while on the field
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_DISABLE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetTarget(function(e,c) return c:IsType(e:GetLabel()) end)
+	e2:SetLabel(ct)
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e2,tp)
 end
-function s.ffilter2(c,fc,sumtype,tp)
-	return c:IsAttribute(ATTRIBUTE_DARK,fc,sumtype,tp) and c:IsSetCard(0x499,fc,sumtype,tp)
+function s.sumlimit(e,c,sump,sumtype,sumpos,targetp)
+	local declared_type=e:GetLabel()
+	if c:IsMonster() then
+		return c:IsType(declared_type)
+	else
+		return c:IsOriginalType(declared_type)
+	end
 end
